40fd6a70758f4214f262ffa502fbee881e7c3bc6
Heja <hejaratsi@gmail.com>
2020-07-01 14:30:36 +0200

    RBF uses Kmeans

---
+++ Project/TestsDEV/RBFKMeans/cmake-build-debug/CMakeFiles/RBFKMeans.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RBFKMeans.dir/main.cpp.obj"
+  "RBFKMeans.exe"
+  "RBFKMeans.exe.manifest"
+  "RBFKMeans.lib"
+  "RBFKMeans.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RBFKMeans.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
