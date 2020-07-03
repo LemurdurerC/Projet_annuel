540f4f007b01c5935fbb1e17e23e27a0d6904788
Heja <hejaratsi@gmail.com>
2020-07-03 15:32:11 +0200

    test side python

--- Project/Tests/RBF_Python/RBF_Test.py
+++ Project/Tests/RBF_Python/RBF_Test.py
@@ -3,9 +3,7 @@ import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
 
-
 if __name__ == "__main__":
-
     path_to_dll = "../../Lib/RBFLib/cmake-build-debug/RBFLib.dll"
 
     my_lib = ctypes.CDLL(path_to_dll)
@@ -13,8 +11,15 @@ if __name__ == "__main__":
     my_lib.create_RBF_model.argtypes = [ctypes.c_int]
     my_lib.create_RBF_model.restype = ctypes.c_void_p
 
-    my_lib.disposeRBF.argtypes = [ctypes.c_void_p,ctypes.c_void_p]
+    my_lib.disposeRBF.argtypes = [ctypes.c_void_p,
+                                  ctypes.c_void_p]
     my_lib.disposeRBF.restype = None
+
+    my_lib.KMeans.argtypes = [ctypes.c_int,
+                              ctypes.POINTER(ctypes.c_double),
+                              ctypes.c_int,
+                              ctypes.c_int]
+    my_lib.KMeans.restype = ctypes.c_void_p
 
     my_lib.RBF_predict_model_Regression.argtypes = [
         ctypes.c_void_p,
@@ -49,4 +54,74 @@ if __name__ == "__main__":
     my_lib.RBF_train_model.restype = None
 
 
-    print("Hello");
+    X = np.array([
+        [1.0, 3.0],
+        [1.0, 4.0],
+        [2.0, 2.0],
+        [2.0, 5.0],
+        [3.0, 3.0],
+
+        [6.0, 7.0],
+        [7.0, 8.0],
+        [8.0, 9.0],
+        [8.0, 5.0],
+        [9.0, 8.0]
+
+    ], dtype='float64')
+
+    Y = np.array([
+        -1,
+        -1,
+        -1,
+        -1,
+        -1,
+
+        1,
+        1,
+        1,
+        1,
+        1
+    ], dtype='float64')
+
+    flattened_X = X.flatten()
+
+    numberOfCluser = 2
+
+    print("Start")
+    myKmeans = my_lib.KMeans(numberOfCluser, X.ctypes.data_as(ctypes.POINTER(ctypes.c_double)), X.shape[0], X.shape[1])
+    print("Done 1")
+    model = my_lib.create_RBF_model(ctypes.c_int(numberOfCluser))
+    print("Done 2")
+
+    print("Before Training the Modelco")
+    for inputs_k in X:
+        print(my_lib.RBF_predict_model_Classification(
+            model,
+            myKmeans,
+            numberOfCluser,
+            1,
+            inputs_k.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
+            len(inputs_k)
+        ))
+    my_lib.RBF_train_model(
+        model,
+        myKmeans,
+        numberOfCluser,
+        flattened_X.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
+        Y.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
+        X.shape[0],
+        X.shape[1],
+        1
+    )
+
+    print("After Training the Model")
+    for inputs_k in X:
+        print(my_lib.RBF_predict_model_Classification(
+            model,
+            myKmeans,
+            numberOfCluser,
+            1,
+            inputs_k.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
+            len(inputs_k)
+        ))
+my_lib.disposeRBF(model,myKmeans)
