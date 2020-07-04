33fa945fe4cdae5ef03c0ee74f3a31d9eafd9b0c
Heja <hejaratsi@gmail.com>
2020-07-04 15:25:33 +0200

    let's deal with the SVM

---
+++ Project/TestsDEV/SVM1/cmake-build-debug/CMakeFiles/SVM1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SVM1.dir/main.cpp.obj"
+  "SVM1.exe"
+  "SVM1.exe.manifest"
+  "SVM1.lib"
+  "SVM1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SVM1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
